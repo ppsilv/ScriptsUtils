edf57fa10ef84a3c100066a9cbce3aec738feba5
pdsilva <ppsilv@gmail.com>
2023-09-18 18:38:29 -0300

    Added parameters treatment

--- vdown.py
+++ vdown.py
@@ -1,3 +1,4 @@
+import sys
 from pytube import YouTube
 
 def Download(link):
@@ -9,6 +10,20 @@ def Download(link):
      print("Has an error downloading")
    print("Download completed succesfull!")
 
-link = input("Please what url?: ")
-Download(link)
+ 
+# total arguments
+n = len(sys.argv)
+if n < 2 :
+	print("usage: vdown <video url>\n\n")
+	exit(1)
+	
+print("Total arguments passed.:", n)
+ 
+# Arguments passed
+print("Name of Python script..:", sys.argv[0])
+print("nUrl...................:", sys.argv[1])
+print("Downloading....\n")    
+    
+#link = input("Please what url?: ")
+Download(sys.argv[1])
 
